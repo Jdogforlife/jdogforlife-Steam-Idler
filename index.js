478d85f48d4a421d7bc17f760f1a75df70de2ecf
Jdogforlife <159442808+Jdogforlife@users.noreply.github.com>
2025-09-21 15:32:52 -0700

    index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var password = process.env.password;
 var shared_secret = process.env.shared;
 
 var games = [230410, 1085660, 216150];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
